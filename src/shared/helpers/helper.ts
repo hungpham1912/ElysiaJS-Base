5374c4069abd2d364c6b44fb23bf7820c3b19ec3
Pham.T.Hung <phamthanh.hung.work@gmail.com>
2023-09-26 15:05:26 +0700

    UODATE

--- src/shared/helpers/helper.ts
+++ src/shared/helpers/helper.ts
@@ -1,6 +1,21 @@
+import { HttpStatus } from "../models/base.model";
+
 export const apiTags = (tags: string, body?: any) => {
   return {
     body,
     detail: { tags: [tags] },
   };
+};
+
+export const Res = (data: any, statusCode?: number) => {
+  try {
+    return new Response(JSON.stringify(data), {
+      status: statusCode ?? HttpStatus.OK,
+    });
+  } catch (error) {
+    console.log("🚀 ~ file: helper.ts:12 ~ Res ~ error:", error);
+    return new Response(JSON.stringify({ error }), {
+      status: HttpStatus.INTERNAL_SERVER_ERROR,
+    });
+  }
 };
